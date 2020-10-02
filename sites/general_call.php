f00b16c8d6aad6e866cdd1dd5437549b78392511
mxmueller <63018152+mxmueller@users.noreply.github.com>
2020-10-02 11:18:14 -0700

    Create general_call.php

---
+++ sites/general_call.php
@@ -0,0 +1,17 @@
+<php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMS | General Call</title>
+</head>
+<body>
+    
+
+<h1>Michael Du Hurensohn</h1>
+</body>
+</html>
+
+?>
