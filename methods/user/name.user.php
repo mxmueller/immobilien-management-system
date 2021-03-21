229d2e595d259d7ea81c504b244dc98395139bbf
Maximilian Müller <maximilian.mueller@atrivio.de>
2021-03-21 19:00:06 +0100

    cleaning up

--- methods/user/name.user.php
+++ methods/user/name.user.php
@@ -1,18 +1,16 @@
 <?php
 
-    $logged_in_user = $_SESSION['user_id'];
+$logged_in_user = $_SESSION['user_id'];
 
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $database = "ims_local_instance";
-    // Create connection
-    $connection = new mysqli($servername, $username, $password, $database);
+$servername = "bngwcgyjxquzsogdpgjv-mysql.services.clever-cloud.com";
+$username = "ubzcxi5aeywhopxg";
+$password = "GkgWQRKJzxDrFquEIQp6";
+$database = "bngwcgyjxquzsogdpgjv";
+// Create connection
+$connection = new mysqli($servername, $username, $password, $database);
 
-    $sql = "SELECT * FROM landlords WHERE LandlordID = $logged_in_user";
-    $result = mysqli_query($connection, $sql);
-    $fetch = mysqli_fetch_row($result);
-    $glued = $fetch['1'] . ' ' . $fetch['2'];  
-    echo $glued;
-
-?>
+$sql = "SELECT * FROM landlords WHERE LandlordID = $logged_in_user";
+$result = mysqli_query($connection, $sql);
+$fetch = mysqli_fetch_row($result);
+$glued = $fetch['1'] . ' ' . $fetch['2'];
+echo $glued;
--- methods/view/zipcode.view.php
+++ methods/view/zipcode.view.php
@@ -1,32 +1,33 @@
 <?php
 
-    $frontend_zipcode = $_POST['zipcodeinsert'];
+$frontend_zipcode = $_POST['zipcodeinsert'];
 
-    if (strlen($frontend_zipcode) > 5) {
-        $frontend_zipcode = substr_replace($frontend_zipcode, "", -1);
-        $city = cityValidator($frontend_zipcode);
-    } else {
-        $city = cityValidator($frontend_zipcode);
-    }
+if (strlen($frontend_zipcode) > 5) {
+    $frontend_zipcode = substr_replace($frontend_zipcode, "", -1);
+    $city = cityValidator($frontend_zipcode);
+} else {
+    $city = cityValidator($frontend_zipcode);
+}
 
-    print_r($city[3]);
+print_r($city[3]);
 
-    function cityValidator($zipcode) {
-        $servername = "127.0.0.1";
-        $username = "root";
-        $password = "";
-        $database = "ims_local_instance";
+function cityValidator($zipcode)
+{
+    $servername = "bngwcgyjxquzsogdpgjv-mysql.services.clever-cloud.com";
+    $username = "ubzcxi5aeywhopxg";
+    $password = "GkgWQRKJzxDrFquEIQp6";
+    $database = "bngwcgyjxquzsogdpgjv";
 
-        // Create connection
-        $connection = new mysqli($servername, $username, $password, $database);
+    // Create connection
+    $connection = new mysqli($servername, $username, $password, $database);
 
-        $sql = "SELECT * FROM zipcodes WHERE zipcode = $zipcode";
-        $result = mysqli_query($connection, $sql);
-        return mysqli_fetch_row($result);
-    }
+    $sql = "SELECT * FROM zipcodes WHERE zipcode = $zipcode";
+    $result = mysqli_query($connection, $sql);
+    return mysqli_fetch_row($result);
+}
 
-    function startsWith ($string, $startString) { 
-    $len = strlen($startString); 
-    return (substr($string, 0, $len) === $startString); 
-    } 
-?>
+function startsWith($string, $startString)
+{
+    $len = strlen($startString);
+    return (substr($string, 0, $len) === $startString);
+}
